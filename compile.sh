4e0fe4d952bd4e20fc33ca8418832764b89dcb1b
AnimMouse <git@animmouse.eu.org>
2021-04-27 15:31:00 +0800

    Add scripts

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac -cp SweetHome3D-6.5.2.jar:sunflow-0.07.3i.jar -d . ConsolePhotoGenerator.java
---
+++ dependencies.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get install openjdk-8-jre-headless openjdk-8-jdk-headless
